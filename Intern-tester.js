38273c83c50e9dd1d89a98a82e44ecf29736b4d6
Lê Văn Anh Tân <58280404+AnhTan1420@users.noreply.github.com>
2022-05-22 12:44:40 +0700

    Add files via upload

---
+++ Intern-tester.js
@@ -0,0 +1,19 @@
+// untitled.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('PASSWORD PROTECTED', () => {
+    it('test', () => {
+      cy.visit('http://text.local/wp-login.php?redirect_to=http%3A%2F%2Ftext.local%2Fwp-admin%2Fedit.php%3Fpost_type%3Dpage&reauth=1')
+
+      cy.get('#user_login').type(' text')
+      cy.get('#user_pass').type(' text')
+      cy.get('#wp-submit').click()
+      cy.get('#post-22 > td.title.column-title.has-row-actions.column-primary.page-title > div.row-actions > span.view > a').should('be.visible').click({force: true})
+      cy.get('#pwbox-22').type('1234')
+      cy.get('#wp--skip-link--target > div.wp-container-7.entry-content.wp-block-post-content > form > p:nth-child(2) > input[type=submit]').click()
+    })
+  })
